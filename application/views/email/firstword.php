726291781e3b61b5dfbdefbf441bfc8195f7802e
Preetham MS <talkwithpreetham@gmail.com>
2014-03-21 00:29:47 +0530

    Few changes, timzone fix(not perfect ) First word mass mail in admin

---
+++ application/views/email/firstword.php
@@ -0,0 +1,19 @@
+<p><span style="font-family:verdana,geneva,sans-serif">Hi {fullname},</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">You have signed up for ProVocab, but you have not added your first word.</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">ProVocab is a portal to log the words you have learnt daily and revise the words learnt. ProVocab makes learning new words simple and helps keep track of progress. All the data are stored in cloud and can be accessed from anywhere.</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">To Start using, add the first word here, ( You need to login with facebook. Do not worry, we will never post on your wall )</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">http://provocab.com/word/insert</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">Once added, you will be able to view the words at</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">http://provocab.com/word//today</span></p>
+
+<p><span style="font-family:verdana,geneva,sans-serif">We wish you all the best in Vocab learning,<br />
+ProVocab team.</span></p>
+
+<pre>
+<span style="font-family:courier new,courier,monospace">This mail is sent by ProVocab. For any queries and help, please mail us at help@provocab.com. To Unsubscribe, send an email to help@provocab.com with title &#39;Unsubscribe&#39;.</span></pre>
